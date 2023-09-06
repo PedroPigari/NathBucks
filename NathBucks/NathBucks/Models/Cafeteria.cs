3a30f4f291495152bb003e91202e928448ac0823
PedroPigari <86801664+PedroPigari@users.noreply.github.com>
2023-09-06 09:19:47 -0300

    Add files via upload

---
+++ NathBucks/NathBucks/Models/Cafeteria.cs
@@ -0,0 +1,13 @@
+﻿namespace NathBucks.Models
+{
+    public class Cafeteria
+    {
+        public int IdCafeteria { get; set; }
+
+        public string Funcionarios{ get; set; }
+
+        public int EstoqueCafe { get; set; }
+
+        public int EstoqueCopos { get; set; }
+    }
+}
---
+++ NathBucks/NathBucks/Models/Cardapio.cs
@@ -0,0 +1,15 @@
+﻿namespace NathBucks.Models
+{
+    public class Cardapio
+    {
+        public int IdCardapio { get; set; }
+
+        public string TipoDrink { get; set; }
+
+        public string TamanhoDrink { get; set; }
+
+        public string DescricaoDrink { get; set; }
+
+        public float PrecoDrink { get; set; }
+    }
+}
