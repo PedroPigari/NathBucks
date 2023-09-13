9f550fcbbca0ea208ffa8cd65a0307e18ecf93f2
PedroPigari <pedrobpigari@gmail.com>
2023-09-13 11:10:03 -0300

    Criando persistência de dados, views e controllers

--- NathBucks/NathBucks/Models/Cardapio.cs
+++ NathBucks/NathBucks/Models/Cardapio.cs
@@ -1,15 +1,25 @@
-﻿namespace NathBucks.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NathBucks.Models
 {
+    [Table("Cardapio")]
     public class Cardapio
     {
+        [Key]
+        [Column]
         public int IdCardapio { get; set; }
 
+        [Column]
         public string TipoDrink { get; set; }
 
+        [Column]
         public string TamanhoDrink { get; set; }
 
+        [Column]
         public string DescricaoDrink { get; set; }
 
+        [Column]
         public float PrecoDrink { get; set; }
     }
 }
--- NathBucks/NathBucks/Program.cs
+++ NathBucks/NathBucks/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using NathBucks.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection"));
+});
 
 var app = builder.Build();
 
